ad729867d82f20b0155aeb1e1682886746514e08
Mathis PEINGNEZ <mathispeingnez@iMac-Mathis-PEIGNEZ.local>
2020-02-10 15:07:25 +0100

    Express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const PORT = 8080;
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
